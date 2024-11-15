bca494527bbc5d6f9b6943183d88d771c705e348
RaiDevX8 <charanrai193@gmail.com>
2024-11-15 20:21:12 +0530

    fix : map marker

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,6 +1,7 @@
+import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-
+import logo from "@/assets/marker.png"
 interface Coordinates {
   lat: number;
   lon: number;
@@ -19,20 +20,23 @@ const Map = ({ coordinates }: MapProps) => {
     return null;
   };
 
+  // Custom icon definition
+  const customIcon = new L.Icon({
+    iconUrl: logo, // URL to custom marker icon
+    iconSize: [35, 35], // Custom icon size
+    iconAnchor: [17, 35], // Anchor the icon to the center-bottom
+    popupAnchor: [0, -35], // Popup position relative to the icon
+    shadowSize: [50, 64], // Custom shadow size
+    shadowAnchor: [22, 64] // Custom shadow anchor
+  });
+
   return (
     <div className="w-full h-96">
       {coordinates?.lat !== undefined && coordinates?.lon !== undefined ? (
-        <MapContainer style={{ height: "100%", width: "100%", zIndex: 49 }}>
+        <MapContainer style={{ height: "100%", width: "100%" }}>
           <MapViewUpdater />
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <TileLayer
-            url={`https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=${import.meta.env.VITE_OPENWEATHER_API_KEY}`}
-          />
-          <TileLayer
-            url={`https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=${import.meta.env.VITE_OPENWEATHER_API_KEY}`}
-          />
-          {/* Marker with default icon */}
-          <Marker position={[coordinates.lat, coordinates.lon]}>
+          <Marker position={[coordinates.lat, coordinates.lon]} icon={customIcon}>
             <Popup>Your Location</Popup>
           </Marker>
         </MapContainer>
